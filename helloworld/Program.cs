33cde65868d92d6514b11611e6684f9d7635c4a2
drxslvk <dr.hyperfomovich@vk.com>
2025-10-19 18:40:41 +0700

    initial commit

---
+++ helloworld/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+class Program {
+	static void Main() {
+		Console.Write("Hello World!\nHy everyone from NSPU!\n");
+		Console.WriteLine("This is my 1st C# app <3");
+	}
+}
---
+++ hw.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Program {
+	static void Main() {
+		Console.Write("Hello world!\nHi everyone!");
+		Console.WriteLine("This is my 1st programm on C# <3");
+	}
+}
