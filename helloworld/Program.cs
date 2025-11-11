9ab5e0da9155adaa9099c893f930f54c702e6ea1
drxslvk <dr.hyperfomovich@vk.com>
2025-11-12 03:19:44 +0700

    Минорное обновление

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -2,7 +2,6 @@
 
 class Program {
 	static void Main() {
-		Console.Write("Hello World!\nHy everyone from NSPU!\n");
-		Console.WriteLine("This is my 1st C# app <3");
+		Console.Write("Hello World");
 	}
 }
